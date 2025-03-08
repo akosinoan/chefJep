33ee1906b8bcac549139c7cce9176384197ca09e
noan babao <noan.babao@gmail.com>
2025-03-08 06:28:10 -0600

    try change banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta property="og:image" content="<generated>" />
-      <meta property="og:image:type" content="<generated>" />
-      <meta property="og:image:width" content="<generated>" />
-      <meta property="og:image:height" content="<generated>" />
+     
       </head>
       <body
         className={`${inter.className}`} > 
